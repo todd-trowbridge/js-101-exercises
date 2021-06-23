4faaaa5000b6cded6d26d8f64a5d89055bef374f
Todd Trowbridge <todd.trowbridge@icloud.com>
2021-06-23 14:46:14 -0400

    finished

--- exercises/level7.js
+++ exercises/level7.js
@@ -3,3 +3,11 @@
 // times as the input number
 //
 // Name the function "finalFunction"
+function finalFunction(num){
+  arr = [];
+  for (let index = 1; index <= num; index++) {
+    const element = num;
+    arr.push('string')
+  }
+  return arr;
+}
